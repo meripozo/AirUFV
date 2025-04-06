4f5f7eb7fa27238849eb0cbdbd3df2a598131fe9
angela <9302227@alumnos.ufv.es>
2025-04-06 21:28:16 +0200

    We added the attirbutes and string comparison (error) of airport. Also added the consolewirtelines of the AddAircraft to add the info attributes

--- PW_1/Airport.cs
+++ PW_1/Airport.cs
@@ -85,7 +85,45 @@ namespace AirportSimulation
                 string[] lines = File.ReadAllLines(filePath);
                 foreach (string line in lines)
                 {
-                    //
+                    // ID;State;Distance;Speed;Type;FuelCapacity;FuelConsumption;AdditionalData
+                    string[] parts = line.Split(';');
+                    if (parts.Length < 8)
+                    {
+                        Console.WriteLine("File format error.");
+                        return false;
+                    }
+                    string id = parts[0];
+                    if (!Enum.TryParse(parts[1], out AircraftStatus status))
+                    {
+                        Console.WriteLine("State parsing error.");
+                        return false;
+                    }
+
+                    int distance = int.Parse(parts[2]);
+                    int speed = int.Parse(parts[3]);
+                    string type = parts[4];
+                    double fuelCapacity = double.Parse(parts[5]);
+                    double fuelConsumption = double.Parse(parts[6]);
+
+                    double currentFuel = fuelCapacity; // maxed fuel before starting flight
+
+                    // StringComparison for upper and lower case letter compatibility
+                    if ("Commercial", StringComparison.OrdinalIgnoreCase)
+                    {
+                        int numPassengers = int.Parse(parts[7]);
+                        Aircrafts.Add(new CommercialAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, numPassengers));
+                    }
+                    else if ("Cargo", StringComparison.OrdinalIgnoreCase)
+                    {
+                        double maxLoad = double.Parse(parts[7]);
+                        Aircrafts.Add(new CargoAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, maxLoad));
+                    }
+                    else if ("Private", StringComparison.OrdinalIgnoreCase)
+                    {
+                        string owner = parts[7];
+                        Aircrafts.Add(new PrivateAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, owner));
+                    }
+
                     else
                     {
                         Console.WriteLine("Unknown aircraft type.");
@@ -111,11 +149,44 @@ namespace AirportSimulation
             Console.WriteLine(" 2. Cargo");
             Console.WriteLine(" 3. Private");
 
+            int selectedType = Convert.ToInt32(Console.ReadLine());
+            if (selectedType > 3 || selectedType < 1)
+            {
+                Console.WriteLine("Invalid selection.");
+                return;
+            }
 
-            //aircraft atributes are still missing 
+            // ID
+            Console.Write("Enter Aircraft ID: ");
+            string id = Console.ReadLine();
+
+            // State
+            Console.Write("Enter initial state (InFlight, Waiting, Landing, OnGround): ");
+            string stateInput = Console.ReadLine();
+            if (!Enum.TryParse(stateInput, out AircraftStatus status)) // PREGUNTAR A MOISÉS
+            {
+                Console.WriteLine("Invalid state.");
+                return;
+            }
+            
+            // Distance
+            Console.Write("Enter distance from airport (km): ");
+            int distance = Convert.ToInt32(Console.ReadLine());
+
+            // Speed
+            Console.Write("Enter speed (km/h): ");
+            int speed = Convert.ToInt32(Console.ReadLine());
+
+            // Fuel Capacity
+            Console.Write("Enter fuel capacity (liters): ");
+            double fuelCapacity = Convert.ToDouble(Console.ReadLine());
+
+            // Fuel Consumption
+            Console.Write("Enter fuel consumption (liters/km): ");
+            double fuelConsumption = Convert.ToDouble(Console.ReadLine());
+            double currentFuel = fuelCapacity; // maxed tank before flight
+
         }
-
-
 
 
         //create load 
