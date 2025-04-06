2043e85582c1598d7eb88ef2c296ac6ce87c36c1
Jaime Ordovás <150004573+Jaimeordovas@users.noreply.github.com>
2025-04-06 18:11:12 +0200

    advance ticker

--- PW_1/Airport.cs
+++ PW_1/Airport.cs
@@ -4,28 +4,70 @@ namespace AirportSimulation
 {
     public abstract class Airport
     {
+        public List<Runway> Runways { get; set; }
         public List<Aircraft> Aircrafts {get; set;}
-        //once we create the runway class, we have to create a runway list
 
         public Airport()
         {
+            Runways = new List<Runway>();
             Aircrafts = new List<Aircraft>();
             //add runway list
         }
 
-        public void ShowStatus() //al ser void creo que no devuelve nada no?
+        // Adds runway to runways list
+        public void AddRunway(Runway runway)
         {
+            Runways.Add(runway);
+        }
+        
+        public void ShowStatus()
+        {
+            Console.WriteLine("Runway Status:");
+            foreach (var runway in Runways)
+            {
+                Console.WriteLine(runway.ToString());
+            }
+            
             //show aircraft status 
             Console.WriteLine("\nAircraft Status:");
             foreach (var aircraft in Aircrafts)
             {
                 Console.WriteLine(aircraft.ToString());
             }
-
-            //add runway status 
         }
 
-        //create class for the 15 minute tick
+        // Advance simulation by one tick (15 mins)
+        public void AdvanceTick()
+        {
+            foreach (var aircraft in Aircrafts)
+            {
+                if (aircraft.Status == AircraftStatus.OnGround)
+                {
+                    aircraft.UpdateTick();
+                }
+            }
+
+            foreach (var aircraft in Aircrafts)
+            {
+                if (aircraft.Status == AircraftStatus.Waiting)
+                {
+                    foreach (var runway in Runways)
+                    {
+                        if (runway.Status == RunwayStatus.Free)
+                        {
+                            runway.RequestRunway(aircraft);
+                            return;
+                        }
+                    }
+                }
+            }
+
+            // Update runways to process landing ticks
+            foreach (var runway in Runways)
+            {
+                runway.UpdateTick();
+            }
+        }
 
         //create class load from file
 
