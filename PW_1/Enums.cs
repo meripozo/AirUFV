9c135fd22ab1c630d3f2c8ba45958e5e2d591a9c
Jaime Ordovás <150004573+Jaimeordovas@users.noreply.github.com>
2025-04-06 11:20:05 +0200

    Enums for runway and aircraft status

---
+++ PW_1/Enums.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AirportSimulation
+{
+    public enum AircraftStatus
+    {
+        InFlight,
+        Waiting,
+        Landing,
+        OnGround
+    }
+
+    public enum RunwayStatus
+    {
+        Free,
+        Occupied
+    }
+}
