6c0e16bbd628392aedf59e84c55e1d6121eafd0a
Jaime Ordovás <150004573+Jaimeordovas@users.noreply.github.com>
2025-04-07 19:05:26 +0200

    used streamreader to read lines one by one (load from file works perfect)

--- PW_1/Airport.cs
+++ PW_1/Airport.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace AirportSimulation
 {
     public class Airport
     {
         public List<Runway> Runways { get; set; }
-        public List<Aircraft> Aircrafts {get; set;}
+        public List<Aircraft> Aircrafts { get; set; }
 
         public Airport()
         {
             Runways = new List<Runway>();
             Aircrafts = new List<Aircraft>();
-            //add runway list
         }
 
         // Adds runway to runways list
@@ -19,7 +22,8 @@ namespace AirportSimulation
         {
             Runways.Add(runway);
         }
-        
+
+        // Display the status of all runways and aircraft
         public void ShowStatus()
         {
             Console.WriteLine("Runway Status:");
@@ -27,8 +31,7 @@ namespace AirportSimulation
             {
                 Console.WriteLine(runway.ToString());
             }
-            
-            //show aircraft status 
+
             Console.WriteLine("\nAircraft Status:");
             foreach (var aircraft in Aircrafts)
             {
@@ -39,6 +42,7 @@ namespace AirportSimulation
         // Advance simulation by one tick (15 mins)
         public void AdvanceTick()
         {
+            // Update each aircraft (skip if OnGround)
             foreach (var aircraft in Aircrafts)
             {
                 if (aircraft.Status != AircraftStatus.OnGround)
@@ -47,6 +51,7 @@ namespace AirportSimulation
                 }
             }
 
+            // Attempt to assign waiting aircraft to free runways so it can land
             foreach (var aircraft in Aircrafts)
             {
                 if (aircraft.Status == AircraftStatus.Waiting)
@@ -72,96 +77,116 @@ namespace AirportSimulation
         // Load aircraft from a CSV file
         // If return true = flights loaded successfully
         // If return flase = flights not loaded successfully
+        // public bool LoadAircraftFromFile(string filePath)
+        // {
+        //     if (!File.Exists(filePath))
+        //     {
+        //         Console.WriteLine("File does not exist.");
+        //         return false;
+        //     }
+
+        //     try
+        //     {
+        //         StreamReader fileSr = File.OpenText(filePath);  //ahora en la variable fileSr tengo todo el documento
+
+        //         string line = fileSr.ReadLine(); //esto es para que se salte la primero línea del documento cuando lo lea
+
+
+        //         while ((line = fileSr.ReadLine()) != null) //bucle que va de línea en línea, hasta que ya no encuentra líneas. 
+        //         {
+        //             //Reemplazar puntos por comas en la línea
+
+        //             string[] lineReading = line.Split(';'); //cuando lee el ; divide el contenido de la línea y lo guarda en el array
+        //             //esta función divide el contenido de line en parámetros
+        //             //(se me crea una array con distintos strings de los productos que hay en cada línea ) 
+        //             string[] list = line.Split(";");
+
+        //             if (list[4] == "Commercial")
+        //             {
+        //                 //Console.WriteLine(double.Parse(list[3]));
+
+        //                 Aircrafts.Add(new CommercialAircraft(     //así añadimos a la list aircrafts un aircrafts de cada tipo, con sus correspondientes atributos
+        //                     list[0],
+        //                     (AircraftStatus)Enum.Parse(typeof(AircraftStatus), list[1]),
+        //                     int.Parse(list[2]),
+        //                     int.Parse(list[3]),
+        //                     double.Parse(list[5]),
+        //                     double.Parse(list[4]),
+        //                     double.Parse(list[6]),
+        //                     int.Parse(list[7])));
+        //                 Console.Clear();
+        //             }
+        //             else if (list[4] == "Cargo")
+        //             {
+        //                 Aircrafts.Add(new CargoAircraft(
+        //                     list[0],
+        //                     (AircraftStatus)Enum.Parse(typeof(AircraftStatus), list[1]),
+        //                     int.Parse(list[2]),
+        //                     int.Parse(list[3]),
+        //                     int.Parse(list[4]),
+        //                     int.Parse(list[5]),
+        //                     int.Parse(list[6]),
+        //                     int.Parse(list[7])));
+        //                 Console.Clear();
+        //             }
+        //             else if (list[4] == "Private")
+        //             {
+        //                 Aircrafts.Add(new PrivateAircraft(
+        //                     list[0],
+        //                     (AircraftStatus)Enum.Parse(typeof(AircraftStatus), list[1]),
+        //                     int.Parse(list[2]),
+        //                     int.Parse(list[3]),
+        //                     int.Parse(list[4]),
+        //                     int.Parse(list[5]),
+        //                     int.Parse(list[6]),
+        //                     list[7]));
+        //                 Console.Clear();
+        //             }
+
+        //             else { Console.WriteLine("There is a problem with the file or with the information"); Console.ReadKey();  }
+        //         }
+        //         return false;
+        //     } 
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine("Error loading file: " + ex.Message);
+        //         Console.ReadLine();
+        //         return false;
+        //     }
+        // }
+
         public bool LoadAircraftFromFile(string filePath)
         {
             if (!File.Exists(filePath))
             {
                 Console.WriteLine("File does not exist.");
+                Console.ReadLine();
                 return false;
             }
 
             try
             {
-                StreamReader fileSr = File.OpenText(filePath);  //ahora en la variable fileSr tengo todo el documento
+                StreamReader sr = File.OpenText(filePath);  //ahora en la variable fileSr tengo todo el documento
 
-            string line = fileSr.ReadLine(); //esto es para que se salte la primero línea del documento cuando lo lea
+                string line = sr.ReadLine(); // se salte la primera línea del documento al leerlo
 
-
-            while ((line = fileSr.ReadLine()) != null) //bucle que va de línea en línea, hasta que ya no encuentra líneas. 
-            {
-                //Reemplazar puntos por comas en la línea
-
-                string[] lineReading = line.Split(';'); //cuando lee el ; divide el contenido de la línea y lo guarda en el array
-                //esta función divide el contenido de line en parámetros
-                //(se me crea una array con distintos strings de los productos que hay en cada línea ) 
-                string[] list = line.Split(";");
-
-                if (list[4] == "Commercial")
-                {
-                    Console.WriteLine(double.Parse(list[3]));
-
-                    Aircrafts.Add(new CommercialAircraft(     //así añadimos a la list aircrafts un aircrafts de cada tipo, 
-                                                              //con sus correspondientes atributos
-                        list[0],
-                        list[1],
-                        int.Parse(list[2]),
-                        int.Parse(list[3]),
-                        int.Parse(list[5]),
-                        int.Parse(list[6]),
-                        int.Parse(list[7])));
-                    Console.Clear();
-                }
-                else if (list[4] == "Cargo")
-                {
-                    Aircrafts.Add(new CargoAircraft(
-                        list[0],
-                        list[1],
-                        int.Parse(list[2]),
-                        int.Parse(list[3]),
-                        int.Parse(list[4]),
-                        int.Parse(list[5]),
-                        int.Parse(list[6]),
-                        int.Parse(list[7])));
-                    Console.Clear();
-                }
-                else if (list[4] == "Private")
-                {
-                    Aircrafts.Add(new PrivateAircraft(
-                        list[0],
-                        list[1],
-                        int.Parse(list[2]),
-                        int.Parse(list[3]),
-                        int.Parse(list[4]),
-                        int.Parse(list[5]),
-                        int.Parse(list[6]),
-                        list[7]));
-                    Console.Clear();
-                }
-
-                else { Console.WriteLine("There is a problem with the file or with the information"); Console.ReadKey(); }
-
-
-
-
-
-
-                //string[] lines = File.ReadAllLines(filePath);
-                foreach (string line in lines)
+                while ((line = sr.ReadLine()) != null) //bucle que va de línea en línea, hasta que ya no encuentra líneas. 
                 {
                     // ID;State;Distance;Speed;Type;FuelCapacity;FuelConsumption;AdditionalData
                     string[] parts = line.Split(';');
                     if (parts.Length < 8)
                     {
                         Console.WriteLine("File format error.");
+                        Console.ReadLine();
                         return false;
                     }
                     string id = parts[0];
                     if (!Enum.TryParse(parts[1], out AircraftStatus status))
                     {
                         Console.WriteLine("State parsing error.");
+                        Console.ReadLine();
                         return false;
                     }
-
                     int distance = int.Parse(parts[2]);
                     int speed = int.Parse(parts[3]);
                     string type = parts[4];
@@ -186,10 +211,10 @@ namespace AirportSimulation
                         string owner = parts[7];
                         Aircrafts.Add(new PrivateAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, owner));
                     }
-
                     else
                     {
                         Console.WriteLine("Unknown aircraft type.");
+                        Console.ReadLine();
                         return false;
                     }
                 }
@@ -197,21 +222,21 @@ namespace AirportSimulation
             catch (Exception ex)
             {
                 Console.WriteLine("Error loading file: " + ex.Message);
+                Console.ReadLine();
                 return false;
             }
             return true;
         }
 
-
-        // we Add an aircraft manually with the users console input
+        // Adds an aircraft manually with the users console input
         public void AddAircraft()
         {
             // Aircraft Type
             Console.WriteLine("Select an Aircraft Type:");
-            Console.WriteLine(" 1. Commercial");
+            Console.WriteLine(" 1. Commercia");
             Console.WriteLine(" 2. Cargo");
             Console.WriteLine(" 3. Private");
-
+            
             int selectedType = Convert.ToInt32(Console.ReadLine());
             if (selectedType > 3 || selectedType < 1)
             {
@@ -229,17 +254,18 @@ namespace AirportSimulation
             if (!Enum.TryParse(stateInput, out AircraftStatus status)) // PREGUNTAR A MOISÉS
             {
                 Console.WriteLine("Invalid state.");
+                Console.ReadLine();
                 return;
             }
 
-            // Distance
             int distance = 0;
             if (stateInput == "InFlight")
             {
+                // Distance
                 Console.Write("Enter distance from airport (km): ");
                 distance = Convert.ToInt32(Console.ReadLine());
             }
-
+            
             // Speed
             Console.Write("Enter speed (km/h): ");
             int speed = Convert.ToInt32(Console.ReadLine());
@@ -253,30 +279,32 @@ namespace AirportSimulation
             double fuelConsumption = Convert.ToDouble(Console.ReadLine());
             double currentFuel = fuelCapacity; // maxed tank before flight
 
-
-            // Additional Data depending on the aircraft type: own specific attributes
+            // Additional Data
             switch(selectedType)
             {
                 case 1:
                     Console.Write("Enter number of passengers: ");
                     int numPassengers = Convert.ToInt32(Console.ReadLine());
                     Aircrafts.Add(new CommercialAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, numPassengers));
-                    Console.WriteLine("Commercial Aircraft Successfully added!");
+                    Console.WriteLine("Commercial Aircrfat Successfully added!");
+                    Console.ReadLine();
                     break;
                 case 2:
                     Console.Write("Enter maximum load (kg): ");
                     double maxLoad = Convert.ToDouble(Console.ReadLine());
                     Aircrafts.Add(new CargoAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, maxLoad));
-                    Console.WriteLine("Cargo Aircraft Successfully added!");
+                    Console.WriteLine("Cargo Aircrfat Successfully added!");
+                    Console.ReadLine();
                     break;
                 case 3:
                     Console.Write("Enter owner name: ");
                     string owner = Console.ReadLine();
                     Aircrafts.Add(new PrivateAircraft(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel, owner));
-                    Console.WriteLine("Private Aircraft Successfully added!");
+                    Console.WriteLine("Private Aircrfat Successfully added!");
+                    Console.ReadLine();
                     break;
                 default:
-                    Console.WriteLine("Invalid Aircraft type selection.");
+                    Console.WriteLine("Invalid aircraft type selection.");
                     break;
             }
         }
