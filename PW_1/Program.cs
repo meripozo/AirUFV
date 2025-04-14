5b668f34410bf5305f8e31f1dfeaa9a4bbc7308e
meripozo <9306433@alumnos.ufv.es>
2025-04-14 14:10:23 +0200

    fixed program error (MainManu function deleted)

--- PW_1/Program.cs
+++ PW_1/Program.cs
@@ -5,51 +5,48 @@ namespace AirportSimulation
     {
         public static void Main(string[] args)
         {
-            public void MainMenu()
+            Simulator simulator = new Simulator(); //we instantiate Simulator and Airport classes
+            Airport airport = new Airport();
+
+            bool exit = false;
+            while (!exit)
             {
-                Simulator simulator = new Simulator(); //we instantiate Simulator and Airport classes
-                Airport airport = new Airport();
+                Console.Clear();
+                
+                Console.WriteLine("\n╔═══════════════════════════════════════╗");
+                Console.WriteLine("║    Airport Landing Simulation Menu    ║");
+                Console.WriteLine("║                                       ║");
+                Console.WriteLine("║ 1. Load flights from file             ║");
+                Console.WriteLine("║ 2. Add a flight manually              ║");
+                Console.WriteLine("║ 3. Start simulation (Manual)          ║");
+                Console.WriteLine("║ 4. Exit                               ║");
+                Console.WriteLine("╚═══════════════════════════════════════╝");
+                Console.Write("Select an option: ");
 
-                bool exit = false;
-                while (!exit)
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
                 {
-                    Console.Clear();
-                    
-                    Console.WriteLine("\n╔═══════════════════════════════════════╗");
-                    Console.WriteLine("║    Airport Landing Simulation Menu    ║");
-                    Console.WriteLine("║                                       ║");
-                    Console.WriteLine("║ 1. Load flights from file             ║");
-                    Console.WriteLine("║ 2. Add a flight manually              ║");
-                    Console.WriteLine("║ 3. Start simulation (Manual)          ║");
-                    Console.WriteLine("║ 4. Exit                               ║");
-                    Console.WriteLine("╚═══════════════════════════════════════╝");
-                    Console.Write("Select an option: ");
-
-                    int option = Convert.ToInt32(Console.ReadLine());
-
-                    switch (option)
-                    {
-                        case 1:
-                            Console.Write("Enter file path: ");
-                            string filePath = Console.ReadLine();
-                            bool result = airport.LoadAircraftFromFile(filePath);
-                            if (result) Console.WriteLine("Flights loaded successfully.");
-                            Console.ReadLine();
-                            break;
-                        case 2:
-                            airport.AddAircraft();
-                            break;
-                        case 3:
-                            simulator.RunSimulationManu();
-                            break;
-                        case 4:
-                            exit = true;
-                            break;
-                        default:
-                            Console.WriteLine("Invalid option. Try again.");
-                            Console.ReadLine();
-                            break;
-                    }
+                    case 1:
+                        Console.Write("Enter file path: ");
+                        string filePath = Console.ReadLine();
+                        bool result = airport.LoadAircraftFromFile(filePath);
+                        if (result) Console.WriteLine("Flights loaded successfully.");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        airport.AddAircraft();
+                        break;
+                    case 3:
+                        simulator.RunSimulationManu();
+                        break;
+                    case 4:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Try again.");
+                        Console.ReadLine();
+                        break;
                 }
             }
         }
